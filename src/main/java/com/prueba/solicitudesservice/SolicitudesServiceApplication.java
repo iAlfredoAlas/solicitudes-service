5a46feac29de67cfb0410c1dadfa292849d64071
Alfredo Alas <alfredo.alas14@gmail.com>
2026-04-08 15:31:45 -0600

    add project files

---
+++ src/main/java/com/prueba/solicitudesservice/SolicitudesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.solicitudesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolicitudesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prueba/solicitudesservice/SolicitudesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.solicitudesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
